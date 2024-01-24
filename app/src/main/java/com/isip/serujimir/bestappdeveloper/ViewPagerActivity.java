190a8ef45e3ded5a62cb920a27697ec694af1dda
kris <sleptcovakristina89@mail.ru>
2024-01-24 15:06:23 +0900

    qweqwe

--- app/src/main/java/com/isip/serujimir/bestappdeveloper/ViewPagerActivity.java
+++ app/src/main/java/com/isip/serujimir/bestappdeveloper/ViewPagerActivity.java
@@ -1,6 +1,7 @@
 package com.isip.serujimir.bestappdeveloper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
@@ -10,5 +11,6 @@ public class ViewPagerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pager);
+
     }
 }
